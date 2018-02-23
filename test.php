3e7def755b8ea19d5dfd133532924e1821ad4c26
john <alokjohntoppo@gmail.com>
2018-02-23 12:43:57 +0530

    manish abc

---
+++ test.php
@@ -0,0 +1,39 @@
+ <?php phpinfo();
+<h1> hello this john </h1>
+" hello this is supriya ....felling boar "
+The removal of aliens who pose  
+[      	 ] shall be ICE’s high [      	 ]
+ 
+These aliens include  [      	 ]
+engaged in or suspected of
+[      	 ] or who otherwise pose
+ 
+[      	 ] aliens convicted of [      	 ]
+particular emphasis [      	 ] and repeat
+ 
+aliens [      	 ] who participated [      	 ]
+[       	] subject to outstanding [      	 ]
+who otherwise pose [      	 ]  to public safety.
+ 
+ 
+Aliens who are [       	]  otherwise
+obstruct [       	]  fugitive aliens,
+in descending priority as [      	 ]
+aliens who pose [      	 ]  security; 
+[      	 ]  or who otherwise pose [      	 ]        	
+the community; [      	 ] other than [      	 ];
+and [      	 ] who have not been [      	 ];
+ 
+aliens who reenter [      	 ]  in descending
+priority as [      	 ]  aliens who pose [      	 ]  
+previously removed [      	 ] who other-
+wise pose [      	 ]  to the community; 
+previously removed [      	 ] who have not been
+convicted of [      	 ] who obtain
+admission or status by [      	 ] ;
+otherwise  [  
+
+
+
+
+ ?>
